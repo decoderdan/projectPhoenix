873c1c088cd91f8bb1806dd6d7850586e571aedd
decoderdan <daniel5.davies@live.uwe.ac.uk>
2013-05-08 13:17:01 -0700

    Drastically cleaned and reduced code.

--- SoftNodes/pilot/src/xbox_pilot/xbox_pilot.cpp
+++ SoftNodes/pilot/src/xbox_pilot/xbox_pilot.cpp
@@ -31,6 +31,12 @@ int main( int argc, char **argv )
 	return 0;
 }
 
+float constrain(float x, float min, float max) {
+	if (x < min) return min;
+	if (x > max) return max;
+	return x;
+}
+
 float map(float x, float in_min, float in_max, float out_min, float out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -38,87 +44,24 @@ float map(float x, float in_min, float in_max, float out_min, float out_max)
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
 
-
-
-		/* Need to map it here... */
-		/* 0 = Left X Axis
-			 1 = Left Y Axis
-		*/
-		
-		/* Convert x/y to angle and distance */
-		float angle = atan2(-joy->axes[0],joy->axes[1]); //Switch X and Y to offset by 90 degrees
-		angle = (angle >= 0 ? angle : (2*M_PI + angle)) * 360 / (2*M_PI); //From PI->0->-PI To degrees
-		float distance = sqrt(pow(-joy->axes[0],2)+pow(joy->axes[1],2));
-		distance = (distance >= 1 ? 1 : distance);
-		distance = (distance < 0.12 ? 0 : distance); //Bit of a dead zone
-		distance = map(distance, 0.12, 1, 0, 1); //Re-map
-		distance = (distance < 0 ? 0 : distance);
-		
-		if ((angle >= 0) && (angle <= 45)) {
-			//Forward to Right-Forward
-			motorCfg.front_right = map(angle, 0, 45, -100, 0);
-			motorCfg.back_right = 100;
-			motorCfg.back_left = map(angle, 0, 45, 100, 0);
-			motorCfg.front_left = -100;
-		}
-		else if (angle <= 90) {
-			//Right-Forward to Strafe-Right
-			motorCfg.front_right = map(angle, 45, 90, 0, 100);
-			motorCfg.back_right = 100;
-			motorCfg.back_left = map(angle, 45, 90, 0, -100);
-			motorCfg.front_left = -100;
-		}
-		else if (angle <= 135) {
-			//Strafe-Right to Backwards-Right
-			motorCfg.front_right = 100;
-			motorCfg.back_right = map(angle, 90, 135, 100, 0);
-			motorCfg.back_left = -100;
-			motorCfg.front_left = map(angle, 90, 135, -100, 0);
-		}
-		else if (angle <= 180) {
-			//Backwards-Right to Backwards
-			motorCfg.front_right = 100;
-			motorCfg.back_right = map(angle, 135, 180, 0, -100);
-			motorCfg.back_left = -100;
-			motorCfg.front_left = map(angle, 135, 180, 0, 100);
-		}
-		else if (angle <= 225) {
-			//Backwards to Backwards-Left
-			motorCfg.front_right = map(angle, 180, 225, 100, 0);
-			motorCfg.back_right = -100;
-			motorCfg.back_left = map(angle, 180, 225, -100, 0);
-			motorCfg.front_left = 100;
-		}
-		else if (angle <= 270) {
-			//Backwards-Left to Strafe-Left
-			motorCfg.front_right = map(angle, 225, 270, 0, -100);
-			motorCfg.back_right = -100;
-			motorCfg.back_left = map(angle, 225, 270, 0, 100);
-			motorCfg.front_left = 100;
-		}
-		else if (angle <= 315) {
-			//Strafe-Left to Forward-Left
-			motorCfg.front_right = -100;
-			motorCfg.back_right = map(angle, 270, 315, -100, 0);
-			motorCfg.back_left = 100;
-			motorCfg.front_left = map(angle, 270, 315, 100, 0);
-		}
-		else {
-			//Forward-Left to Forward
-			motorCfg.front_right = -100;
-			motorCfg.back_right = map(angle, 315, 360, 0, 100);
-			motorCfg.back_left = 100;
-			motorCfg.front_left = map(angle, 315, 360, 0, -100);
-		}
+	/* 0 = Left X Axis
+		 1 = Left Y Axis
+	*/
 	
-		//Multiply by distances for speed
-		motorCfg.front_right*=distance;
-		motorCfg.back_right*=distance;
-		motorCfg.front_left*=distance;
-		motorCfg.back_left*=distance;
+	float rads = atan2(joy->axes[1], -joy->axes[0]); //X & Y to rads
+	float distance = constrain(sqrt(pow(-joy->axes[0],2)+pow(joy->axes[1],2)),0,1); //Get distance
+	distance = (distance < 0.12 ? 0 : distance); //Bit of a dead zone
+	distance = constrain(map(distance, 0.12, 1, 0, 1), 0, 1); //Re-map so it's smooth from deadzone outwards
 	
-		//Leave out depth for now
-		motorCfg.front = 0;
-		motorCfg.back = 0;	
-		motorMsg.publish(motorCfg);
+	motorCfg.front_right = (int8_t)(constrain(140 * cos(rads+M_PI/4), -100, 100) * distance);
+	motorCfg.front_left = (int8_t)(constrain(-140 * cos(M_PI/4-rads), -100, 100) * distance);
+	motorCfg.back_left = (int8_t)(constrain(-140 * cos(rads+M_PI/4), -100, 100) * distance);
+	motorCfg.back_right = (int8_t)(constrain(140 * cos(M_PI/4-rads), -100, 100) * distance);
+
+	//Leave out depth for now
+	motorCfg.front = 0;
+	motorCfg.back = 0;	
+
+	motorMsg.publish(motorCfg);
+
 }
