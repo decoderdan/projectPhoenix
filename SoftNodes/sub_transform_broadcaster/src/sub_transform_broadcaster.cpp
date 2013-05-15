429991604bbc62599834faea5764334bf7731275
decoderdan <daniel5.davies@live.uwe.ac.uk>
2013-05-15 18:32:43 +0100

    Fixed sub layout (just needs the correct measurements) and added laser beams

--- SoftNodes/sub_transform_broadcaster/src/sub_transform_broadcaster.cpp
+++ SoftNodes/sub_transform_broadcaster/src/sub_transform_broadcaster.cpp
@@ -24,48 +24,60 @@ void imuCallBack(const custom_msg::IMUData& data) {
   ROS_INFO("Got IMU Message");
   
   static tf::TransformBroadcaster br;
-  static float imu_x, imu_y;
-	static float fr_roll, fl_roll, br_roll, bl_roll;
-	
+	static float imu_x, imu_y = 0;
+	static float fr_roll, fl_roll, br_roll, bl_roll = 0;
+
   tf::Transform imu_tr;
+  tf::Transform svp_tr;
+  tf::Transform sonar_tr;
   tf::Transform front_right_tr; //Front Right Motor
   tf::Transform back_right_tr; //Back Right Motor
   tf::Transform front_left_tr; //Front Left Motor
   tf::Transform back_left_tr; //Back Left Motor
   
-  imu_x += (1.0/50.0)*(data.vel_x+data.acc_x);
-  imu_y += (1.0/50.0)*(data.vel_y+data.acc_y);
-	
-	//imu_tr.setOrigin( tf::Vector3(0.1,0.5,0) );
-	//imu_tr.setRotation( tf::createQuaternionFromRPY(0,0,0) );
-	
-	imu_tr.setOrigin( tf::Vector3(imu_x, imu_y, z.data) );
+  /* Configure motor rotations */
+	fl_roll+=(fl_rot/200);
+	fr_roll+=(fr_rot/200);
+	br_roll+=(br_rot/200);
+	bl_roll+=(bl_rot/200);
+  
+  /* Sonar transformation */
+  sonar_tr.setOrigin(tf::Vector3(-0.12, -0.32, -0.30));
+  sonar_tr.setRotation( tf::createQuaternionFromRPY(0,0,M_PI) );
+  
+  /* SVP transformation */
+  float svp_distance_from_imu = -0.07;
+  svp_tr.setOrigin(tf::Vector3(0.10, -0.15, svp_distance_from_imu));
+  svp_tr.setRotation( tf::createQuaternionFromRPY(0,0,0) );
+
+  /* IMU transformation */
+	imu_x += data.vel_x * 1/50;//0.5 * data.acc_x * pow((1/50), 2);
+	imu_y += data.vel_y * 1/50;//0.5 * data.acc_y * pow((1/50), 2);
+	imu_tr.setOrigin( tf::Vector3(imu_x, imu_y, z.data-svp_distance_from_imu) );
 	imu_tr.setRotation( tf::createQuaternionFromRPY(data.pitch*(M_PI/180), data.roll*(M_PI/180), -data.yaw*(M_PI/180)) );
+	//imu_tr.setRotation( tf::createQuaternionFromRPY(0,0,0) );
+
+	/* Motors */
+	front_right_tr.setOrigin( tf::Vector3(0.20, 0.15, -0.10) );
+	front_right_tr.setRotation(  tf::createQuaternionFromRPY(0, fr_roll, M_PI/6) );
+	front_left_tr.setOrigin( tf::Vector3(-0.20, 0.15, -0.10) );
+	front_left_tr.setRotation(  tf::createQuaternionFromRPY(0, fl_roll, -M_PI/6) );
+	back_right_tr.setOrigin( tf::Vector3(0.20, -0.40, -0.10) );
+	back_right_tr.setRotation(  tf::createQuaternionFromRPY(0, br_roll, 5*M_PI/6) );
+	back_left_tr.setOrigin( tf::Vector3(-0.20, -0.40, -0.10) );
+	back_left_tr.setRotation(  tf::createQuaternionFromRPY(0, bl_roll, 7*M_PI/6) );
 	
-	/* Configure motor rotations */
-	fl_roll+=(fl_rot/100);
-	fr_roll+=(fr_rot/100);
-	br_roll+=(br_rot/100);
-	bl_roll+=(bl_rot/100);
-	
-	/* Configure the front right motor transform */
-	front_right_tr.setOrigin( tf::Vector3(0.20, 0.15, -0.08) );
-	front_right_tr.setRotation(  tf::createQuaternionFromRPY(fr_roll, 0, 0) );
-	/* Configure the back right motor transform */
-	back_right_tr.setOrigin( tf::Vector3(0.20, -0.40, -0.08) );
-	back_right_tr.setRotation(  tf::createQuaternionFromRPY(br_roll, 0, 0) );
-	/* Configure the front left motor transform */
-	front_left_tr.setOrigin( tf::Vector3(-0.20, 0.15, -0.08) );
-	front_left_tr.setRotation(  tf::createQuaternionFromRPY(fl_roll, 0, 0) );
-	/* Configure the back left motor transform */
-	back_left_tr.setOrigin( tf::Vector3(-0.20, -0.40, -0.08) );
-	back_left_tr.setRotation(  tf::createQuaternionFromRPY(bl_roll, 0, 0) );
-	
+	/* Publish our transformations */
 	//IMU relative to the world.
 	br.sendTransform(tf::StampedTransform(imu_tr, ros::Time::now(), "/world", "/imu"));
+	//SVP relative to IMU
+	br.sendTransform(tf::StampedTransform(svp_tr, ros::Time::now(), "/imu", "/svp"));
+	//SVP relative to the IMU
+	br.sendTransform(tf::StampedTransform(sonar_tr, ros::Time::now(), "/imu", "/sonar"));
+	//Thrusters relative to IMU
 	br.sendTransform(tf::StampedTransform(front_right_tr, ros::Time::now(), "/imu", "/front_right_thruster"));
-	br.sendTransform(tf::StampedTransform(back_right_tr, ros::Time::now(), "/imu", "/back_right_thruster"));
 	br.sendTransform(tf::StampedTransform(front_left_tr, ros::Time::now(), "/imu", "/front_left_thruster"));
+	br.sendTransform(tf::StampedTransform(back_right_tr, ros::Time::now(), "/imu", "/back_right_thruster"));
 	br.sendTransform(tf::StampedTransform(back_left_tr, ros::Time::now(), "/imu", "/back_left_thruster"));
 }
 
