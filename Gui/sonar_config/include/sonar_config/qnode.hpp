89a322b2a602c5b32b43dccbdb391518f1f85da7
ChrisB <rationalRobots@gmail.com>
2013-05-20 01:25:30 +0100

    created Gui directory, with sonar_config subdir, and designed sonar_config/ui/main_window.ui

---
+++ Gui/sonar_config/include/sonar_config/qnode.hpp
@@ -0,0 +1,71 @@
+/**
+ * @file /include/sonar_config/qnode.hpp
+ *
+ * @brief Communications central!
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
+
+#ifndef sonar_config_QNODE_HPP_
+#define sonar_config_QNODE_HPP_
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <ros/ros.h>
+#include <string>
+#include <QThread>
+#include <QStringListModel>
+
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace sonar_config {
+
+/*****************************************************************************
+** Class
+*****************************************************************************/
+
+class QNode : public QThread {
+    Q_OBJECT
+public:
+	QNode(int argc, char** argv );
+	virtual ~QNode();
+	bool init();
+	bool init(const std::string &master_url, const std::string &host_url);
+	void run();
+
+	/*********************
+	** Logging
+	**********************/
+	enum LogLevel {
+	         Debug,
+	         Info,
+	         Warn,
+	         Error,
+	         Fatal
+	 };
+
+	QStringListModel* loggingModel() { return &logging_model; }
+	void log( const LogLevel &level, const std::string &msg);
+
+signals:
+	void loggingUpdated();
+    void rosShutdown();
+
+private:
+	int init_argc;
+	char** init_argv;
+	ros::Publisher chatter_publisher;
+    QStringListModel logging_model;
+};
+
+}  // namespace sonar_config
+
+#endif /* sonar_config_QNODE_HPP_ */
