9b7a058b2df7fcd1c35ad46b6bd85fab5b1411a8
matthuxford <matt@huxhome.org>
2013-05-31 18:09:16 +0100

    Dan can you look at this code (mission_control). does not compile, not sure why. im still not 100% with TFs.

--- SoftNodes/mission_control/src/mission_control/mission.cpp
+++ SoftNodes/mission_control/src/mission_control/mission.cpp
@@ -3,10 +3,16 @@
 #include "std_msgs/UInt8.h"
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
+#include <sensor_msgs/Joy.h>
 
+void joyCallback(const sensor_msgs::Joy::ConstPtr&);
 void broadcastWPs();
+void manualControl(int);
+void setStartPoint();
 
 int state_selected = 1;
+int x_button = 0;
+int a_button = 0;
 
 int main(int argc, char **argv)
 {
@@ -17,9 +23,22 @@ int main(int argc, char **argv)
   
   ROS_INFO("TEST PRINT");
 
-while (1) {
-  broadcastWPs();
-};
+while (1) 
+{
+  switch(state_selected)
+  {
+  	case 1 : manualControl(1);
+  		   break;
+  		   
+  	case 2 : manualControl(0);
+  		   setStartPoint();
+  		   break;
+  		   
+  	case 3 : manualControl(0);
+  		   broadcastWPs(); 
+  		   break;
+  }
+}
   return 0;
 }
 
@@ -33,16 +52,69 @@ void broadcastWPs()
 
 	wp.setOrigin( tf::Vector3(5.0, 0.0, 0.0) );
 	wp.setRotation( tf::createQuaternionFromRPY(0.0, 0.0, 0.0) );
-	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/world", "/square_WP1" ));
+	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/square_origin", "/square_WP1" ));
 
 	wp.setOrigin( tf::Vector3(5.0, 5.0, 0.0) );
-	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/world", "/square_WP2" ));
+	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/square_origin", "/square_WP2" ));
 
 	wp.setOrigin( tf::Vector3(0.0, 5.0, 0.0) );
-	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/world", "/square_WP3" ));
+	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/square_origin", "/square_WP3" ));
 
 	wp.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
-	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/world", "/square_WP4" ));
+	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/square_origin", "/square_WP4" ));
 
 }
 
+void manualControl(int on)
+{
+	if (on)
+	{
+		//In here needs to send a message to activate manual controls
+	}
+	
+	else
+	{
+		//In here needs to send a message to deactivate manual controls
+	}
+}
+
+void setStartPoint()
+{
+	 tf::StampedTransform transform;
+	 
+       try
+       {
+         wp.lookupTransform("/imu", "/square_origin",  //this line does not compile. I dont know what is wrong
+                                  ros::Time(0), transform);
+       }
+       
+       catch (tf::TransformException ex)
+       {
+         ROS_ERROR("%s",ex.what());
+       }
+       
+       state_selected = 3;	//now origin waypoint has been saved start moving to the waypoints
+}
+
+void joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+{
+
+  x_button = joy->buttons[2];		//X button
+  a_button = joy->buttons[1];
+
+     if(x_button)
+     {
+	  if(x_button)
+	  {
+	  	ROS_INFO("The sub is moving in a square [%d]", x_button);
+		state_selected = 2;	//first set origin waypoint then move in a square
+	  }
+
+	  if(a_button)
+	  {
+	  	ROS_INFO("The sub is manually controlled [%d]", x_button);
+		state_selected = 1;
+	  }
+
+    }
+}
