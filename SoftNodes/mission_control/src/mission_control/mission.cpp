c0fae3c906ab6bb53a720e50e69b8e007e40cb9a
Daniel Davies <daniel5.davies@live.uwe.ac.uk>
2013-06-17 18:15:43 +0100

    REVERTED mission control

--- SoftNodes/mission_control/src/mission_control/mission.cpp
+++ SoftNodes/mission_control/src/mission_control/mission.cpp
@@ -4,27 +4,18 @@
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include <sensor_msgs/Joy.h>
-#include <custom_msg/TargetVector.h>
 
 void broadcastWPs();
 void manualControl(int);
 void setStartPoint();
 void joyCallback(const sensor_msgs::Joy::ConstPtr&);
-float map(float x, float in_min, float in_max, float out_min, float out_max);
 
 int state_selected = 1;
 int x_button = 0;
 int a_button = 0;
-bool got_vals = false;
-
-//int cur_wp = 1;
-
-float x_analog = 0;
-float l_analog = 0;
-float r_analog = 0;
 
 tf::StampedTransform origin_tf; //Stores our origin tf
-ros::Publisher target_publisher;
+
 
 int main(int argc, char **argv)
 {
@@ -34,17 +25,12 @@ int main(int argc, char **argv)
 
 	/* Subscribe to joystick */
 	ros::Subscriber joy_sub = n.subscribe<sensor_msgs::Joy>("joy", 10, joyCallback);
-  target_publisher = n.advertise<custom_msg::TargetVector>("vector", 1000);
-	
+  
   ROS_INFO("Mission Control Node Started - Subscribed to joystick");
 
-
-setStartPoint();
 while (ros::ok()) 
 {
-//  ros::spinOnce(); //Allow joystick callback
-broadcastWPs();
-/*  switch(state_selected)
+  switch(state_selected)
   {
   	case 1 : manualControl(1);
   		   break;
@@ -58,15 +44,15 @@ broadcastWPs();
   		   break;
   }
   
-*/}
+  ros::spinOnce(); //Allow joystick callback
+}
   return 0;
 }
 
 void broadcastWPs()
 {
   static tf::TransformBroadcaster broadcaster;
-	//std::ostringstream oss;
-	
+
 	std::cout << "broadcast the waypoints for a square" << std::endl;
 
 	//First, broadcast our final destination relative to the world (This is our current position)
@@ -77,28 +63,13 @@ void broadcastWPs()
 	
 	wp.setOrigin( tf::Vector3(5.0, 0.0, 0.0) );
 	wp.setRotation( tf::createQuaternionFromRPY(0.0, 0.0, 0.0) );
-//	oss << "/wp" << cur_wp++;
 	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/square_origin", "/wp1" ));
-	//oss.str("");
-	//oss.clear();
-	
 	wp.setOrigin( tf::Vector3(5.0, -5.0, 0.0) );
-	//oss << "/wp" << cur_wp++;
-  broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/square_origin", "wp2" ));
-	//oss.str("");
-	//oss.clear();
-	
+	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/square_origin", "/wp2" ));
 	wp.setOrigin( tf::Vector3(0.0, -5.0, 0.0) );
-	//oss << "/wp" << cur_wp++;
-	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/square_origin", "wp3" ));
-	//oss.str("");
-	//oss.clear();
-	
+	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/square_origin", "/wp3" ));
 	wp.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
-	//oss << "/wp" << cur_wp++;
-	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/square_origin", "wp4" ));
-	//oss.str("");
-	//oss.clear();
+	broadcaster.sendTransform( tf::StampedTransform( wp, ros::Time::now(), "/square_origin", "/wp4" ));
 }
 
 void manualControl(int on)
@@ -106,17 +77,6 @@ void manualControl(int on)
 	if (on)
 	{
 		//In here needs to send a message to activate manual controls
-		//Spit out motor controls based on xbox controller
-    custom_msg::TargetVector tv;
-    tv.set_y = false;
-		tv.set_z = false;
-    tv.set_pitch = false;
-    tv.set_roll = false;
- 		tv.vector_yaw = map(x_analog, 1.0, -1.0, -180.0, 180.0);
- 		tv.vector_x = (map(r_analog, 1.0, -1.0, 0.0, 100.0));   
- 		std::cout << "Yaw: " << tv.vector_yaw << " X: " << tv.vector_x << std::endl;
-		//Publish
-		target_publisher.publish(tv);
 	}
 	
 	else
@@ -151,10 +111,6 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
   x_button = joy->buttons[2];		//X Button
   a_button = joy->buttons[0];   //A Button
 
-	x_analog = joy->axes[0];
-	l_analog = joy->axes[2];
-	r_analog = joy->axes[5];
-	
 	if(x_button && (state_selected != 3))
 	{
 		ROS_INFO("The sub is moving in a square [%d]", x_button);
@@ -166,9 +122,4 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
 		ROS_INFO("The sub is manually controlled [%d]", x_button);
 		state_selected = 1;
 	}
-}
-
-float map(float x, float in_min, float in_max, float out_min, float out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
