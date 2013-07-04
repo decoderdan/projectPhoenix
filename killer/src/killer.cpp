a04dc0764bc339924470b13500287927343d0b13
chrisB <rationalrobots@googlemail.com>
2013-07-05 00:34:38 +0100

    should all work

--- killer/src/killer.cpp
+++ killer/src/killer.cpp
@@ -8,7 +8,7 @@
 #include <string>
 #include <unistd.h>
 
-#define killer_timeout 240		// timeout in seconds 
+#define killer_timeout 300		// timeout in seconds 
 
 float global_depth;
 
@@ -49,7 +49,7 @@ int main(int argc, char **argv) {
 				already_dived = true;
 			} 
 
-			if(ros::Time::now().toSec() > (dive_time.toSec() + 10)) { 
+			if(ros::Time::now().toSec() > (dive_time.toSec() + killer_timeout)) { 
 			// if timeout && no depth callback recieved
 				ROS_ERROR("killer: error: has been down more than %d, killing pid.launch", killer_timeout);
 				system("killall python /opt/ros/electric/ros/bin/roslaunch launch pid.launch");
