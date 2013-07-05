923773ad8afd854fda5d6312bc24bbac721dd955
Daniel Davies <daniel5.davies@live.uwe.ac.uk>
2013-07-05 06:34:49 +0200

    FIXED THAT MO FO

--- SoftNodes/pilot/src/dead_reckoning_pilot/dead_reckoning_pilot.cpp
+++ SoftNodes/pilot/src/dead_reckoning_pilot/dead_reckoning_pilot.cpp
@@ -3,14 +3,25 @@
 #include <custom_msg/TargetVector.h>
 #include <custom_msg/PIDValues.h>
 
+#define GRABBING_YAW 0
+#define DIVING 1
+#define CENTERING 2
+#define STOPPING 3
+#define TURNING 4
+#define VALIDATING 5
+#define SURFACING 6
+#define FINISHED 7
+
+#define TARGET_DEPTH 1.6
+
 bool got_yaw = false;
 float _current_yaw = 0;
 ros::Time start_time;
-
+ros::Publisher pid_config_publisher;
+	
 void imuCallBack(const custom_msg::IMUData& data) {
     _current_yaw = data.yaw;
     got_yaw = true;
-    //ROS_INFO("Got Yaw");
 }
 
 int main(int argc, char** argv){
@@ -20,7 +31,8 @@ int main(int argc, char** argv){
     float rotate_time = 10;
     float through_gate_time = 25;
 
-    ROS_INFO("%i, %s", argc, argv[1]);
+    int state = GRABBING_YAW;
+
     if (argc != 6) {
         //Print help
         printf("\nArguments are all floats, and should be passed in this order:\n\nDIVE_TIME\nTO_CENTER_TIME\nSTOP_TIME\nROTATE_TIME\nTHROUGH_GATE_TIME\n\n");
@@ -38,129 +50,160 @@ int main(int argc, char** argv){
     ros::NodeHandle n;
     ros::Subscriber imuSub = n.subscribe("imu", 100, imuCallBack);
     ros::Publisher target_publisher = n.advertise<custom_msg::TargetVector>("vector", 1000);
-    ros::Publisher pid_pub = n.advertise<custom_msg::PIDValues>("pidGui", 1000);
+    pid_config_publisher = n.advertise<custom_msg::PIDValues>("pidGui", 1000);    
 
     ROS_INFO("Dive:    %fs\nForward: %fs\nStop:    %fs\nRotate:  %fs\nForward: %fs", dive_time, to_center_time, stop_time, rotate_time, through_gate_time);
 
-    //Wait for yaw
-    while (ros::ok() && (!got_yaw)) ros::spinOnce();
+    while (ros::ok()) {
 
-    //--------------------------------------------------------------
-    ROS_INFO("Read Yaw (%f), Holding and Diving...", _current_yaw);
+	//Set up the custom messages to publish
+	custom_msg::TargetVector tv;
+	custom_msg::PIDValues curPIDConfig;
 
-    custom_msg::PIDValues pid; 
-    custom_msg::TargetVector tv;
-    float start_yaw = _current_yaw; //Save current yaw for later
+	float start_yaw;
 
-    pid.yaw_Kp = 2.0;
-    pid.yaw_Ki = 0.05;
-    pid.yaw_Kd = 0.1;
-    pid.pitch_Kp = 2.0;
-    pid.pitch_Ki = 1.0;
-    pid.pitch_Kd = 0.0;
-    pid.depth_Kp = 400.0;
-    pid.depth_Ki = 1.0;
-    pid.depth_Kd = 0.0;
+	switch (state) {
+		case GRABBING_YAW: 	
+			if (got_yaw) {
+				start_yaw = _current_yaw; //Save current yaw for later
+				state = DIVING;
 
-    tv.vector_yaw = start_yaw;
-    tv.vector_z = 1.6; //Set depth 0.6M
-    tv.set_x = false;
-    tv.set_y = false;
-    tv.set_z = true;
-    tv.set_yaw = true;
-    tv.set_pitch = false;
-    tv.set_roll = false;
+				tv.vector_yaw = start_yaw;
+				tv.vector_z = TARGET_DEPTH; //Set depth
+				tv.set_x = false;
+				tv.set_y = false;
+				tv.set_z = true;
+				tv.set_yaw = true;
+				tv.set_pitch = false;
+				tv.set_roll = false;
 
-    target_publisher.publish(tv);
-//    pid_pub.publish(pid);
+				//Publish out target vector and target yaw.
+				target_publisher.publish(tv);
 
-    start_time = ros::Time::now();
-    while(ros::Time::now().toSec() < (start_time.toSec() + dive_time)) {ros::spinOnce();}
+				ROS_INFO("Got Current Yaw Reading (%f). State = DIVING", _current_yaw);
+			}
+			break;
+		case DIVING:
+			ROS_INFO("Diving to %fM", TARGET_DEPTH);
+			curPIDConfig.yaw_Kp = 2.0;
+			curPIDConfig.yaw_Ki = 0.05;
+			curPIDConfig.yaw_Kd = 0.1;
+			curPIDConfig.pitch_Kp = 2.0;
+			curPIDConfig.pitch_Ki = 1.0;
+			curPIDConfig.pitch_Kd = 0.0;
+			curPIDConfig.depth_Kp = 400.0;
+			curPIDConfig.depth_Ki = 1.0;
+			curPIDConfig.depth_Kd = 0.0;
+			pid_config_publisher.publish(curPIDConfig);
 
-    //--------------------------------------------------------------
-    ROS_INFO("Moving to center...");
-    // send a forward 80% command
-    tv.vector_x = 80; //80% speed
-    tv.set_x = true;
-    tv.set_y = false;
-    tv.set_z = false;
-    tv.set_yaw = true;
-    tv.set_pitch = false;
-    tv.set_roll = false;
-    target_publisher.publish(tv);
-    start_time = ros::Time::now();
-    while(ros::Time::now().toSec() < (start_time.toSec() + to_center_time)) {ros::spinOnce();}
+			//Set up a timer
+		        start_time = ros::Time::now();
+			state = CENTERING;
+			break;
+		case CENTERING: 
+			//Check the timer
+			if (ros::Time::now().toSec() >= (start_time.toSec() + dive_time)) {
+				ROS_INFO("Driving forward for %f seconds", to_center_time);				
 
-    //--------------------------------------------------------------
-    ROS_INFO("Stopping...");
-    // send a stop command
-    tv.set_x = true;
-    tv.set_y = false;
-    tv.set_z = false;
-    tv.set_yaw = true;
-    tv.set_pitch = false;
-    tv.set_roll = false;
-    tv.vector_x = 0; //0% speed
-    target_publisher.publish(tv);
+				tv.vector_x = 80; //80% speed
+				tv.set_x = true;
+				tv.set_y = false;
+				tv.set_z = false;
+				tv.set_yaw = true;
+				tv.set_pitch = false;
+				tv.set_roll = false;
+				target_publisher.publish(tv);
 
-    start_time = ros::Time::now();
-    while(ros::Time::now().toSec() < (start_time.toSec() + stop_time)) {ros::spinOnce();}
+				start_time = ros::Time::now();
+				state = STOPPING;
+			}
+			break;
+		case STOPPING: 
+			//Check the timer
+			if (ros::Time::now().toSec() >= (start_time.toSec() + to_center_time)) {
+				ROS_INFO("Stopping within %f seconds", stop_time);				
+				tv.set_x = true;
+				tv.set_y = false;
+				tv.set_z = false;
+				tv.set_yaw = true;
+				tv.set_pitch = false;
+				tv.set_roll = false;
+				tv.vector_x = 0; //0% speed
+				target_publisher.publish(tv);
 
-    //--------------------------------------------------------------
-    ROS_INFO("Rotating 90 degrees to Yaw: %f...", (start_yaw + 90));
-    tv.vector_yaw = start_yaw + 90.0;
-    tv.set_x = false;
-    tv.set_y = false;
-    tv.set_z = false;
-    tv.set_yaw = true;
-    tv.set_pitch = false;
-    tv.set_roll = false;
-    target_publisher.publish(tv);
+				start_time = ros::Time::now();
+				state = TURNING;
+			}
+			break;
+		case TURNING:
+			//Check the timer
+			if (ros::Time::now().toSec() >= (start_time.toSec() + stop_time)) {
+				ROS_INFO("Turning to %f", (start_yaw + 90.0));
+				tv.vector_yaw = start_yaw + 90.0;
+				tv.set_x = false;
+				tv.set_y = false;
+				tv.set_z = false;
+				tv.set_yaw = true;
+				tv.set_pitch = false;
+				tv.set_roll = false;
+				target_publisher.publish(tv);
 
-    start_time = ros::Time::now();
-    while(ros::Time::now().toSec() < (start_time.toSec() + rotate_time)) {ros::spinOnce();}
+				start_time = ros::Time::now();
+				state = VALIDATING;
+			}
+			break;
+		case VALIDATING:
+			//Check the timer
+			if (ros::Time::now().toSec() >= (start_time.toSec() + rotate_time)) {
+				ROS_INFO("Validating");
+				tv.vector_x = 80; //80% speed
+				tv.set_x = true;
+				tv.set_y = false;
+				tv.set_z = false;
+				tv.set_yaw = true;
+				tv.set_pitch = false;
+				tv.set_roll = false;
+				target_publisher.publish(tv);
 
-    //--------------------------------------------------------------
-    ROS_INFO("Moving forward...");
-    tv.vector_x = 80; //80% speed
-    tv.set_x = true;
-    tv.set_y = false;
-    tv.set_z = false;
-    tv.set_yaw = true;
-    tv.set_pitch = false;
-    tv.set_roll = false;
-    target_publisher.publish(tv);
+				start_time = ros::Time::now();
+				state = SURFACING;
+			}
+			break; 
+		case SURFACING:
+			//Check the timer
+			if (ros::Time::now().toSec() >= (start_time.toSec() + through_gate_time)) {
+				ROS_INFO("Surfacing");
+				tv.vector_x = 0; //0% speed
+				tv.vector_z = 0.10;
+				tv.set_x = true;
+				tv.set_y = false;
+				tv.set_z = true;
+				tv.set_yaw = false;
+				tv.set_pitch = false;
+				tv.set_roll = false;
 
-    start_time = ros::Time::now();
-    while(ros::Time::now().toSec() < (start_time.toSec() + through_gate_time)) {ros::spinOnce();}
-
-    //--------------------------------------------------------------
-    ROS_INFO("Stopping & Resurfacing");
-   // send a stop command
-    tv.vector_x = 0; //0% speed
-    tv.vector_z = 0.10;
-    tv.set_x = true;
-    tv.set_y = false;
-    tv.set_z = true;
-    tv.set_yaw = false;
-    tv.set_pitch = false;
-    tv.set_roll = false;
-
-    target_publisher.publish(tv);
-
-    start_time = ros::Time::now();
-    while(ros::Time::now().toSec() < (start_time.toSec() + 1)) {ros::spinOnce();}
-
-    pid.yaw_Kp = 0.0;
-    pid.yaw_Ki = 0.0;
-    pid.yaw_Kd = 0.0;
-    pid.pitch_Kp = 0.0;
-    pid.pitch_Ki = 0.0;
-    pid.pitch_Kd = 0.0;
-    pid.depth_Kp = 0.0;
-    pid.depth_Ki = 0.0;
-    pid.depth_Kd = 0.0;
- //   pid_pub.publish(pid);
+				target_publisher.publish(tv);
+		    		
+				state = FINISHED;
+			}
+			break;
+		case FINISHED:
+			curPIDConfig.yaw_Kp = 0.0;
+			curPIDConfig.yaw_Ki = 0.0;
+			curPIDConfig.yaw_Kd = 0.0;
+			curPIDConfig.pitch_Kp = 0.0;
+			curPIDConfig.pitch_Ki = 0.0;
+			curPIDConfig.pitch_Kd = 0.0;
+			curPIDConfig.depth_Kp = 0.0;
+			curPIDConfig.depth_Ki = 0.0;
+			curPIDConfig.depth_Kd = 0.0;
+			pid_config_publisher.publish(curPIDConfig);
+			ros::spinOnce();
+			return 1;
+			break;
+	}
+	ros::spinOnce();
+    }
 
     return 0;
 };
