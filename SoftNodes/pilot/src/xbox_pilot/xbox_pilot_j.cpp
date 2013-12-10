0eb17b1cd18a984205d078a11a1155cd4f773a2b
jimblebee <jgkillick@hotmail.co.uk>
2013-12-10 15:00:11 +0000

    output_value test

--- SoftNodes/pilot/src/xbox_pilot/xbox_pilot_j.cpp
+++ SoftNodes/pilot/src/xbox_pilot/xbox_pilot_j.cpp
@@ -227,7 +227,8 @@
 	   depth_previous_error = depth_error;
 	   depth_output = (depth_Kp*depth_error) + (depth_Ki*depth_integral) + (depth_Kd*depth_derivative);
 
-       motorCfg.front = (int8_t)(constrain((depth_output), -25, 25));
+	   motorCfg.front = depth_output;
+       //motorCfg.front = (int8_t)(constrain((depth_output), -25, 25));
 	   motorCfg.back = (int8_t)(constrain((depth_output), -25, 25));	
 	
 if(depth_output > 0)
