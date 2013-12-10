d188207ef399f0180f3e2d1f574345a6e84c2f13
jimblebee <jgkillick@hotmail.co.uk>
2013-12-10 13:55:53 +0000

    xbox_pilot with only depth

--- SoftNodes/pilot/src/xbox_pilot/xbox_pilot_j.cpp
+++ SoftNodes/pilot/src/xbox_pilot/xbox_pilot_j.cpp
@@ -42,9 +42,9 @@
 	float dt = 0.02;
 
  float depth_rate = 0.05; 		//m per second
- float depth_Kp = 10;			//3 variables for depth PID
- float depth_Ki = 5;
- float depth_Kd = 1;
+ float depth_Kp = 400;			//3 variables for depth PID
+ float depth_Ki = 1;
+ float depth_Kd = 0;
  float depth_input = 0;			//depth value recieved from the SVP
  float depth_target_raw = 0;	
  float depth_target = 0;		//desired depth value
@@ -53,6 +53,7 @@
  float depth_integral = 0;
  float depth_derivative = 0;
 ////
+
 
  /** *********************************************** **/
  /** Name: vectorCallBack                            **/
